8986f66d9f6561db3e74e5eab2793c95ca48461e
Kathryn Icuspit <60863018+kathrynicuspit@users.noreply.github.com>
2023-03-07 08:54:51 +0800

    update other pages

--- ci4/app/Views/pages/about.php
+++ ci4/app/Views/pages/about.php
@@ -41,10 +41,10 @@
 		<div class="bx bx-menu" id="menu-icon"></div>
 		
 		<ul class="navbar">
-			<li><a href="new.php">Home</a></li>
-			<li><a href="about.html">About Me</a></li>
-			<li><a href="piano.php">Piano</a></li>
-			<li><a href="gallery.php">Gallery</a></li>
+			<li><a href="/~kbicuspit/lab3/ci4/public/new.php">Home</a></li>
+			<li><a href="/~kbicuspit/lab3/ci4/app/Views/pages/about.php">About Me</a></li>
+			<li><a href="/~kbicuspit/lab3/ci4/public/piano.php">Piano</a></li>
+			<li><a href="/~kbicuspit/lab3/ci4/public/gallery.php">Gallery</a></li>
 			<li><a href="/~kbicuspit/lab3/ci4/public/contact.php">Contact Me</a></li>
 		</ul>
 		
