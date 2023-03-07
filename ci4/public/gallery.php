04496b7589c2305f74fbd0b9ff957a7f62aab910
Kathryn Icuspit <60863018+kathrynicuspit@users.noreply.github.com>
2023-03-07 08:56:38 +0800

    Add files via upload

---
+++ ci4/public/gallery.php
@@ -0,0 +1,111 @@
+<!--Eto yung icopy paste sa index.html sa GITHUB-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Personal Profile</title>
+	
+	<link rel="apple-touch-icon" href="img/hmm.png">
+	<link rel="icon" type="image/png" href="img/hmm.png">
+	<link rel="stylesheet" type="text/css" href="css/newstyle.css">
+	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+	
+	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Urbanist:wght@200&display=swap" rel="stylesheet">
+
+
+</head>
+
+
+<body>
+<header>
+		<a href="#" class="logo"><img src="img/logo.png"></a>
+		<div class="bx bx-menu" id="menu-icon"></div>
+		
+		<ul class="navbar">
+			<li><a href="new.html">Home</a></li>
+			<li><a href="about.html">About Me</a></li>
+			<li><a href="piano.html">Piano</a></li>
+			<li><a href="gallery.html">Gallery</a></li>
+			<li><a href="contact.php">Contact Me</a></li>
+		</ul>
+		
+		<ul class="s-navbar">
+				<li><a href="https://github.com/kathrynicuspit" class="btn" target="_blank"><img src="img/gitbtn.png" width="35px" height="35px"></a></li>
+				<li><a href="https://www.facebook.com/kathryn.chaaan/" class="btn" target="_blank"><img src="img/fbbtn.png" width="35px" height="35px"></a></li>
+				<li><a href="https://www.linkedin.com/in/kathryn-ann-icuspit-75659121a/" class="btn" target="_blank"><img src="img/lkdinbtn.png" width="35px" height="35px"></a></li>
+			</ul>
+		</header>
+	
+
+
+</script>
+	
+	<!--GALLERY-->
+	<section id="container-gallery">
+			<div class="row">
+		  <div class="column">
+			<img src="imgg/1.jpg">
+			<img src="imgg/2.jpg">
+			<img src="imgg/3.jpg">
+			<img src="imgg/4.jpg">
+			<img src="imgg/5.jpg">
+			<img src="imgg/6.jpg">
+			<img src="imgg/7.jpg">
+		  </div>
+		  <div class="column">
+			<img src="imgg/8.jpg">
+			<img src="imgg/9.jpg">
+			<img src="imgg/10.jpg">
+			<img src="imgg/1.jpg">
+			<img src="imgg/2.jpg">
+			<img src="imgg/3.jpg">
+		  </div>
+		  <div class="column">
+			<img src="imgg/4.jpg">
+			<img src="imgg/5.jpg">
+			<img src="imgg/6.jpg">
+			<img src="imgg/7.jpg">
+			<img src="imgg/8.jpg">
+			<img src="imgg/9.jpg">
+			<img src="imgg/10.jpg">
+		  </div>
+		  <div class="column">
+			<img src="1.jpg">
+			<img src="2.jpg">
+			<img src="3.jpg">
+			<img src="4.jpg">
+			<img src="5.jpg">
+			<img src="6.jpg">
+		  </div>
+		</div>
+			
+			</section>
+	
+	
+	<!--
+	<div class="hero">
+    <video id="videoplayer" controls width="590" height="470">
+		<source 
+			src="media/videos/vidintro.mp4"
+			type="video/mp4"
+		/>
+	</video>   
+
+
+	   </div>
+-->
+	
+</body>
+
+</html>
+
+	
